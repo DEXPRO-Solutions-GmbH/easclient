08f4cb711199e17ec4e49d296d747e7e2d99fa68
fabiante <fabiante@users.noreply.github.com>
2023-12-12 12:03:45 +0100

    Add ServerClient.PutStore

---
+++ r_put_store_test.go
@@ -0,0 +1,33 @@
+package easclient_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DEXPRO-Solutions-GmbH/easclient"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStoreClient_PutStore(t *testing.T) {
+	testPrelude(t)
+
+	ctx := context.Background()
+	user := easclient.NewUserClaims("test@dexpro.de")
+	ctx = user.SetOnContext(ctx)
+
+	storeName := "random-store"
+
+	err := DefaultServerClient.PutStore(ctx, storeName, &easclient.PutStoreRequest{
+		ConfigurationTemplate: easclient.ConfigurationTemplate{
+			Name: "default",
+			Parameters: []easclient.ConfigurationParameter{
+				{
+					Name:  "STORE_NAME",
+					Value: storeName,
+				},
+			},
+		},
+	})
+
+	require.NoError(t, err)
+}
