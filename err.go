03e7da5141bfc2a325fd91e3714611fa677ada0c
fabiante <fabiante@users.noreply.github.com>
2024-01-19 13:44:08 +0100

    Add isErrorResponse helper to check if EAS responds with error

--- err.go
+++ err.go
@@ -1,5 +1,29 @@
 package easclient
 
+import (
+	"fmt"
+
+	"gopkg.in/resty.v1"
+)
+
 const (
 	HeaderKeyOtrisErr = "X-Otris-Eas-Error"
 )
+
+// isErrorResponse checks if the given response is an error.
+// Returns true and an error object if that is the case.
+func isErrorResponse(res *resty.Response) (bool, error) {
+	if status := res.StatusCode(); status > 300 {
+		return true, fmt.Errorf("unexpected response status %v, expected value < 300", status)
+	}
+	if hasErrHeader(res) {
+		// TODO: unmarshal body to error type?
+		return true, fmt.Errorf("eas responded with error header. status %v is considered an error in that case", res.StatusCode())
+	}
+
+	return false, nil
+}
+
+func hasErrHeader(res *resty.Response) bool {
+	return res.Header().Get(HeaderKeyOtrisErr) == "true"
+}
