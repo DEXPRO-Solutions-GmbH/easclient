6b1d5e2ed1d767b744654e600c33a0db789fcdd4
fabiante <fabiante@users.noreply.github.com>
2024-01-19 13:50:17 +0100

    Add dedicated ResponseErr type

--- err.go
+++ err.go
@@ -10,15 +10,29 @@ const (
 	HeaderKeyOtrisErr = "X-Otris-Eas-Error"
 )
 
+type ResponseErr struct {
+	msg string
+
+	Body []byte
+}
+
+func (r *ResponseErr) Error() string {
+	return r.msg
+}
+
 // isErrorResponse checks if the given response is an error.
-// Returns true and an error object if that is the case.
-func isErrorResponse(res *resty.Response) (bool, error) {
+func isErrorResponse(res *resty.Response) (bool, *ResponseErr) {
 	if status := res.StatusCode(); status > 300 {
-		return true, fmt.Errorf("unexpected response status %v, expected value < 300", status)
+		return true, &ResponseErr{
+			msg:  fmt.Sprintf("unexpected response status %v, expected value < 300", status),
+			Body: res.Body(),
+		}
 	}
 	if hasErrHeader(res) {
-		// TODO: unmarshal body to error type?
-		return true, fmt.Errorf("eas responded with error header. status %v is considered an error in that case", res.StatusCode())
+		return true, &ResponseErr{
+			msg:  fmt.Sprintf("eas responded with error header. status %v is considered an error in that case", res.StatusCode()),
+			Body: res.Body(),
+		}
 	}
 
 	return false, nil
