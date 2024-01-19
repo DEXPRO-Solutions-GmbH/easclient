ebe4367f343bfed962066a5f8a777e05efe0cb2d
fabiante <fabiante@users.noreply.github.com>
2024-01-19 16:20:27 +0100

    Remove empty values in attachment XML template

--- template_record_test.go
+++ template_record_test.go
@@ -71,8 +71,37 @@ func Test_renderPutRecordTemplate(t *testing.T) {
 			<name>Test.pdf</name>
 			<path>ff0351a7-aa00-4269-ab49-fb4172e3193f.tmp</path>
 			<size>56865</size>
-			<register></register>
-			<author></author>
+		</attachment>
+    </record>
+</records>
+`
+		assert.Equal(t, expected, xml)
+	})
+
+	t.Run("record with attachment with minimal fields", func(t *testing.T) {
+		req := &RecordRequest{
+			Fields: map[string]string{
+				"Creditor": "DE123456789",
+				"Debitor":  "DE987654321",
+			},
+			Attachments: []*RecordRequestAttachment{
+				{
+					Name: "Test.pdf",
+					Path: "ff0351a7-aa00-4269-ab49-fb4172e3193f.tmp",
+				},
+			},
+		}
+
+		xml, err := renderRecordTemplate(req)
+		require.NoError(t, err)
+		expected := `<?xml version="1.0"?>
+<records xmlns="http://namespace.otris.de/2010/09/archive/recordExtern">
+    <record>
+		<field name="Creditor">DE123456789</field>
+		<field name="Debitor">DE987654321</field>
+		<attachment>
+			<name>Test.pdf</name>
+			<path>ff0351a7-aa00-4269-ab49-fb4172e3193f.tmp</path>
 		</attachment>
     </record>
 </records>
